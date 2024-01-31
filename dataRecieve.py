d0250d55cd06bcbbc3b72359e7a540a76d7c3709
salman <salmanalijijiga@gmail.com>
2024-01-31 14:26:24 +0400

    commited chagnes

--- dataRecieve.py
+++ dataRecieve.py
@@ -4,35 +4,35 @@ import time
 import requests
 
 # ------------------------server-----------------------------
-import socket
+# import socket
 
-# Define the IP address and port to bind the server
-HOST = '0.0.0.0'  # Replace with the desired IP address
-PORT = 8080  # Replace with the desired port number
+# # Define the IP address and port to bind the server
+# HOST = '0.0.0.0'  # Replace with the desired IP address
+# PORT = 8080  # Replace with the desired port number
 
-def run_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        # Bind the server socket to the specified IP address and port
-        server_socket.bind((HOST, PORT))
+# def run_server():
+#     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+#         # Bind the server socket to the specified IP address and port
+#         server_socket.bind((HOST, PORT))
 
-        # Listen for incoming connections
-        server_socket.listen()
+#         # Listen for incoming connections
+#         server_socket.listen()
 
-        print(f"Server running on {HOST}:{PORT}")
+#         print(f"Server running on {HOST}:{PORT}")
 
-        # Accept incoming connections
-        client_socket, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
+#         # Accept incoming connections
+#         client_socket, client_address = server_socket.accept()
+#         print(f"Connection from {client_address}")
 
-        # Receive data from the client
-        data = client_socket.recv(1024)
-        print(f"Received data: {data.decode('utf-8')}")
+#         # Receive data from the client
+#         data = client_socket.recv(1024)
+#         print(f"Received data: {data.decode('utf-8')}")
 
-        # Close the client socket
-        client_socket.close()
+#         # Close the client socket
+#         client_socket.close()
 
-# Run the server
-run_server()
+# # Run the server
+# run_server()
 # ------------------------server-----------------------------
 
 
@@ -41,11 +41,14 @@ time.sleep(1)
 
 #send command to arduino
 endPoint = "http://10.6.156.26:8000/login/api/token/"
+recievedCmd = "scanUser"
 
 while True:
+    # time.sleep(5)
     # Sending data from py to arduino
     if recievedCmd == "scanUser": #string that is gonna be recieved for the backend when requesting user Id by scanning finger
-        recievedCmd = recievedCmd +'\n'
+        print("Recieved: " + recievedCmd)
+        recievedCmd = recievedCmd +'\r'
         arduinoData.write(recievedCmd.encode())
 
         while (arduinoData.inWaiting()==0):
@@ -55,7 +58,7 @@ while True:
         data = data.strip('\r\n')
         print(data)
 
-        request = requests.post(endPoint, json={"data":data})
+        # request = requests.post(endPoint, json={"data":data})
 
 
     elif recievedCmd == "register":
@@ -65,6 +68,6 @@ while True:
         
 
 
-    else: 
-        print("Unknow command text from the backend, please check typo")
+    # else: 
+    #     print("Unknow command text from the backend, please check typo")
     
